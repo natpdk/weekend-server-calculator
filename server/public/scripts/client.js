cf788a29afaaa325a0bda6a4daa73080cf13a601
Nat Koch <nat.pd.koch@gmail.com>
2024-11-11 07:34:17 -0800

    Set up skeleton of functions. Needed to correct based off of live solve.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,17 @@
 console.log('client.js is sourced!');
+
+let operator;
+
+function setOperator(event) {
+    event.preventDefault()
+}
+
+function handleSubmit(event) {
+    event.preventDefault()
+    console.log("hanleSubmit() activated")
+}
+
+function handleClear(event) {
+    event.preventDefault()
+    console.log("handleClear() activated")
+}
