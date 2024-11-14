ca559126f4218b4c6a9823c079135776681e75b1
Nat Koch <nat.pd.koch@gmail.com>
2024-11-13 16:19:59 -0800

    Added funtion to get history.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,3 +10,19 @@ function setOpt(event, op) {
     operator = op
     console.log("setOpt() is now: ", operator)
 }
+
+function getHistory() {
+    axios({
+        method: "GET",
+        url: "/calculations"
+    })
+        .then((response) => {
+            console.log("Successful GET /calculations: ", response.data)
+            if (response.data.length > 0) {
+                render(response.data)
+            }
+        })
+        .catch((error) => {
+            console.error("Error on GET at /Calculations: ", error)
+        })
+}
