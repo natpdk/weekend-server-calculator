4095a6fc2bb03bb2aad562406abfa8d85769d9dc
Nat Koch <nat.pd.koch@gmail.com>
2024-11-13 16:29:33 -0800

    Added function for the submit button and for clearing the inputs.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -26,3 +26,43 @@ function getHistory() {
             console.error("Error on GET at /Calculations: ", error)
         })
 }
+
+function onSubmit(event) {
+    event.preventDefault()
+    console.log("onSubmit() is a go.")
+
+    const firstNumInput = document.getElementById("firstNumInput").value
+    const secondNumInput = document.getElementById("secondNumInput").value
+
+    let newMath = {
+        firstNum: Number(firstNumInput),
+        secondNum: Number(secondNumInput),
+        operator: operator
+    }
+
+    console.log("New math to senf to the server: ", newMath)
+
+    axios({
+        method: "POST",
+        url: "/calculations",
+        data: newMath
+    })
+        .then((response) => {
+            console.log("Successful POST to /calculations")
+
+            getHistory()
+            handleClear(event)
+        })
+        .catch((error) => {
+            console.error("Error on POST at /calculations: ", error)
+        })
+}
+
+function clearInputs(event) {
+    event.preventDefault()
+    console.log("clearInputs() is a go.")
+
+    document.getElementById("firstNumInput").value = ""
+    document.getElementById("secondNumInput").value = ""
+    operator = undefined
+}
