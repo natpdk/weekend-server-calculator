8f050fe64126289fa41b067ab041dd298d953054
Nat Koch <nat.pd.koch@gmail.com>
2024-11-13 16:16:24 -0800

    Create function to set the operator.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,12 @@
 let operator; //Global variable to holder operator
 
+function onReady() {
+    getHistory()
+}
+onReady()
+
+function setOpt(event, op) {
+    event.preventDefault()
+    operator = op
+    console.log("setOpt() is now: ", operator)
+}
